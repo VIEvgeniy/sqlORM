873b9d99b4aa4fea77a876a3fb827196e182e030
Evgeniy Vakulenko <vakevg@yandex.ru>
2023-01-07 23:44:57 +0300

    modify main and add data file

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import json
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from models import create_tables
+from models import create_tables, Publisher, Book, Shop, Stock, Sale
+
+models = {'publisher': Publisher, 'book': Book, 'shop': Shop, 'stock': Stock, 'sale': Sale}
 
 DSN = "postgresql://postgres:postgres@localhost:5432/bookstore"
 engine = sqlalchemy.create_engine(DSN)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
 create_tables(engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
+# решение подсмотрел в примере реализации и немного переделал как мне понятнее
+with open('fixtures/tests_data.json', 'r') as data_file:
+    data = json.load(data_file)
+for row_data in data:
+    # print(row_data['model'], row_data['pk'], row_data['fields'])
+    row = models[row_data['model']](id=row_data['pk'], **row_data['fields'])
+    session.add(row)
 session.commit()
+
+publisher = input('Введите имя автора: ')
+for s in session.query(Sale).join(Stock.sales).all():
+    print(s)
+
 session.close()
--- models.py
+++ models.py
@@ -3,47 +3,48 @@ from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
 
-class publisher(Base):
+class Publisher(Base):
     __tablename__ = 'publisher'
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
 
-class book(Base):
+class Book(Base):
     __tablename__ = 'book'
 
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=40), unique=True)
-    id_publicher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-    publicher = relationship(publisher, backref='publisher')
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+    publisher = relationship(Publisher, backref='books')
 
-class shop(Base):
+class Shop(Base):
     __tablename__ = 'shop'
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
 
-class stock(Base):
+class Stock(Base):
     __tablename__ = 'stock'
 
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=40), unique=True)
     id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
-    book = relationship(publisher, backref='book')
-    shop = relationship(publisher, backref='shop')
+    book = relationship(Book, backref='stocks')
+    shop = relationship(Shop, backref='stocks')
     count = sq.Column(sq.Integer)
 
-class sale(Base):
+class Sale(Base):
     __tablename__ = 'sale'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer)
+    price = sq.Column(sq.Float)
     date_sale = sq.Column(sq.Date)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
-    stock = relationship(publisher, backref='stock')
+    stock = relationship(Stock, backref='sales')
     count = sq.Column(sq.Integer)
-
+    def __str__(self):
+        return f'{self.stock.book.title:<40}| {self.stock.shop.name:<10}| {self.price:<10}| {self.date_sale}'
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
